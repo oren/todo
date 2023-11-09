7d9adaf3aaaf76da1a3ede0a2141ce615f721e1f
Oren Golan <orengolan@gmail.com>
2023-11-08 20:37:12 -0800

    show content of todo.txt

--- build/app.js
+++ build/app.js
@@ -3,8 +3,18 @@
 const convert = (dir) => {
     console.log('here');
 };
+// if todo.txt exist, show the content
 const listTodo = () => {
-    console.log('list of todos');
+    const fs = require("fs");
+    const exists = fs.existsSync("todo.txt");
+    if (exists) {
+        const data = fs.readFileSync('todo.txt', 'utf8');
+        console.log('todos:');
+        console.log(data);
+    }
+    else {
+        console.log("todo.txt does not exists");
+    }
 };
 // calling without arguments list all todos
 listTodo();
