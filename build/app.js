1dbadfb097efb5f798d07f29aaadb49e0214a1b5
Oren Golan <orengolan@gmail.com>
2023-11-10 05:24:20 -0800

    delete almost working

--- build/app.js
+++ build/app.js
@@ -63,43 +63,9 @@ const command = () => {
         console.log("too many arguments");
     }
 };
-// delete a single todo. not working yet
-function deleteOne(file, todoNumber) {
-    var _a, e_1, _b, _c;
-    return __awaiter(this, void 0, void 0, function* () {
-        const exists = fs.existsSync(file);
-        if (!exists) {
-            console.log("todo.txt does not exists");
-            return;
-        }
-        const fileStream = fs.createReadStream(file);
-        const rl = readline.createInterface({
-            input: fileStream,
-            crlfDelay: Infinity,
-        });
-        try {
-            // Note: we use the crlfDelay option to recognize all instances of CR LF
-            // ('\r\n') in input.txt as a single line break.
-            // Each line in input.txt will be successively available here as `line`.
-            for (var _d = true, rl_1 = __asyncValues(rl), rl_1_1; rl_1_1 = yield rl_1.next(), _a = rl_1_1.done, !_a; _d = true) {
-                _c = rl_1_1.value;
-                _d = false;
-                const line = _c;
-                console.log(line);
-            }
-        }
-        catch (e_1_1) { e_1 = { error: e_1_1 }; }
-        finally {
-            try {
-                if (!_d && !_a && (_b = rl_1.return)) yield _b.call(rl_1);
-            }
-            finally { if (e_1) throw e_1.error; }
-        }
-    });
-}
 // return a single todo
 function printOne(file, todoNumber) {
-    var _a, e_2, _b, _c;
+    var _a, e_1, _b, _c;
     return __awaiter(this, void 0, void 0, function* () {
         const exists = fs.existsSync(file);
         if (!exists) {
@@ -116,8 +82,8 @@ function printOne(file, todoNumber) {
         let index = 1;
         try {
             // Each line in input.txt will be successively available here as `line`.
-            for (var _d = true, rl_2 = __asyncValues(rl), rl_2_1; rl_2_1 = yield rl_2.next(), _a = rl_2_1.done, !_a; _d = true) {
-                _c = rl_2_1.value;
+            for (var _d = true, rl_1 = __asyncValues(rl), rl_1_1; rl_1_1 = yield rl_1.next(), _a = rl_1_1.done, !_a; _d = true) {
+                _c = rl_1_1.value;
                 _d = false;
                 const line = _c;
                 // if it's the todo number I am looking for and not ---, print the line
@@ -132,18 +98,18 @@ function printOne(file, todoNumber) {
                 }
             }
         }
-        catch (e_2_1) { e_2 = { error: e_2_1 }; }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
         finally {
             try {
-                if (!_d && !_a && (_b = rl_2.return)) yield _b.call(rl_2);
+                if (!_d && !_a && (_b = rl_1.return)) yield _b.call(rl_1);
             }
-            finally { if (e_2) throw e_2.error; }
+            finally { if (e_1) throw e_1.error; }
         }
     });
 }
 // if todo.txt exist, show the  titles of all the todos
 function printAll(file) {
-    var _a, e_3, _b, _c;
+    var _a, e_2, _b, _c;
     return __awaiter(this, void 0, void 0, function* () {
         const exists = fs.existsSync(file);
         if (!exists) {
@@ -161,8 +127,8 @@ function printAll(file) {
         let index = 1;
         try {
             // Each line in input.txt will be successively available here as `line`.
-            for (var _d = true, rl_3 = __asyncValues(rl), rl_3_1; rl_3_1 = yield rl_3.next(), _a = rl_3_1.done, !_a; _d = true) {
-                _c = rl_3_1.value;
+            for (var _d = true, rl_2 = __asyncValues(rl), rl_2_1; rl_2_1 = yield rl_2.next(), _a = rl_2_1.done, !_a; _d = true) {
+                _c = rl_2_1.value;
                 _d = false;
                 const line = _c;
                 // if --- -> increment index, okToPrint and continue
@@ -178,6 +144,59 @@ function printAll(file) {
                 }
             }
         }
+        catch (e_2_1) { e_2 = { error: e_2_1 }; }
+        finally {
+            try {
+                if (!_d && !_a && (_b = rl_2.return)) yield _b.call(rl_2);
+            }
+            finally { if (e_2) throw e_2.error; }
+        }
+    });
+}
+// find lines to delete
+// delete them
+// return lines to delete
+function findLinesToDelete(file, todoNumber) {
+    var _a, e_3, _b, _c;
+    return __awaiter(this, void 0, void 0, function* () {
+        const exists = fs.existsSync(file);
+        if (!exists) {
+            console.log("todo.txt does not exists");
+            return;
+        }
+        const fileStream = fs.createReadStream(file);
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity,
+        });
+        // Note: we use the crlfDelay option to recognize all instances of CR LF
+        // ('\r\n') in input.txt as a single line break.
+        let index = 1;
+        let lineNumber = 0;
+        let lines = [];
+        try {
+            // Each line in input.txt will be successively available here as `line`.
+            for (var _d = true, rl_3 = __asyncValues(rl), rl_3_1; rl_3_1 = yield rl_3.next(), _a = rl_3_1.done, !_a; _d = true) {
+                _c = rl_3_1.value;
+                _d = false;
+                const line = _c;
+                // if it's the todo number I am looking for and not ---, print the line
+                if (index === todoNumber) {
+                    if (line === "---") {
+                        lines.push(lineNumber);
+                        break;
+                    }
+                    lines.push(lineNumber);
+                }
+                if (line === "---") {
+                    index += 1;
+                    if (index === todoNumber) {
+                        lines.push(lineNumber);
+                    }
+                }
+                lineNumber += 1;
+            }
+        }
         catch (e_3_1) { e_3 = { error: e_3_1 }; }
         finally {
             try {
@@ -185,6 +204,29 @@ function printAll(file) {
             }
             finally { if (e_3) throw e_3.error; }
         }
+        return lines;
+    });
+}
+function deleteOne(fileName, todoNumber) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const linesToDelete = yield findLinesToDelete(fileName, todoNumber);
+        console.log(linesToDelete);
+        const removeLines = (data, lines = []) => {
+            return data
+                .split('\n')
+                .filter((val, idx) => lines.indexOf(idx) === -1)
+                .join('\n');
+        };
+        fs.readFile(fileName, 'utf8', (err, data) => {
+            if (err)
+                throw err;
+            // remove the first line and the 5th and 6th lines in the file
+            fs.writeFile(fileName, removeLines(data, linesToDelete), 'utf8', function (err) {
+                if (err)
+                    throw err;
+                console.log(`Task ${todoNumber} was deleted`);
+            });
+        });
     });
 }
 command();
--- src/app.ts
+++ src/app.ts
@@ -58,32 +58,6 @@ const command = () {
 	}
 }
 
-// delete a single todo. not working yet
-async function deleteOne(file: string, todoNumber: number) {
-	const exists = fs.existsSync(file)
-
-	if (!exists) {
-		console.log("todo.txt does not exists")
-		return
-	}
-
-  const fileStream = fs.createReadStream(file);
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  });
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
-
-	// Each line in input.txt will be successively available here as `line`.
-  for await (const line of rl) {
-
-		console.log(line)
-		// if it's the todo number I am looking for and not ---, print the line
-  }
-}
-
 // return a single todo
 async function printOne(file: string, todoNumber: number) {
 	const exists = fs.existsSync(file)
@@ -159,6 +133,81 @@ async function printAll(file: string) {
 			okToPrint = false
 		}
   }
+}
+
+// find lines to delete
+// delete them
+
+// return lines to delete
+async function findLinesToDelete(file: string, todoNumber: number) {
+	const exists = fs.existsSync(file)
+
+	if (!exists) {
+		console.log("todo.txt does not exists")
+		return
+	}
+
+  const fileStream = fs.createReadStream(file);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+	let index = 1
+	let lineNumber = 0
+	let lines = []
+
+
+	// Each line in input.txt will be successively available here as `line`.
+  for await (const line of rl) {
+
+		// if it's the todo number I am looking for and not ---, print the line
+		if(index === todoNumber) {
+			if (line === "---") {
+				lines.push(lineNumber)
+				break
+			}
+
+			lines.push(lineNumber)
+		}
+
+		if (line === "---") {
+			index += 1
+
+			if(index === todoNumber) {
+				lines.push(lineNumber)
+			}
+		}
+
+		lineNumber += 1
+  }
+
+	return lines
+}
+
+async function deleteOne(fileName: string, todoNumber: number) {
+	const linesToDelete = await findLinesToDelete(fileName, todoNumber)
+	console.log(linesToDelete)
+
+	const removeLines = (data, lines = []) => {
+			return data
+					.split('\n')
+					.filter((val, idx) => lines.indexOf(idx) === -1)
+					.join('\n');
+	}
+
+	fs.readFile(fileName, 'utf8', (err, data) => {
+			if (err) throw err
+
+			// remove the first line and the 5th and 6th lines in the file
+			fs.writeFile(fileName, removeLines(data, linesToDelete), 'utf8', function(err) {
+					if (err) throw err
+					console.log(`Task ${todoNumber} was deleted`)
+			});
+	})
 }
 
 command()
