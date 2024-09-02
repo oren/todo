7cf25cc5e55df8915b4f7ba41bad8ffffebc12ee
oren <orengolan@gmail.com>
2024-09-01 18:50:49 -0700

    pass first test

--- go/todo/todo.go
+++ go/todo/todo.go
@@ -3,6 +3,7 @@ package todo
 import (
     "fmt"
     "errors"
+    "os"
 )
 
 // Hello returns a greeting for the named person.
@@ -10,6 +11,14 @@ func Add(todo string) (string, error) {
     if todo == "" {
         return "", errors.New("empty todo")
     }
+
+    data := []byte("hello")
+	err := os.WriteFile("todo.md", data, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Added new todo: %v", todo)
     return message, nil
